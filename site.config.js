35a05d13fbbe7fe3668fa1188e1219867b7d1bfc
Zhenyang Li <yeskate@gmail.com>
2021-07-31 22:20:41 +0800

    update config

--- site.config.js
+++ site.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'd60b418829a7472f8e2c7a023b0ff700',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Zhenyang',
+  domain: 'note.zhenyang.li',
+  author: 'zhenyang',
 
   // open graph metadata (optional)
   description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  socialImageTitle: 'zhenyangt',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
